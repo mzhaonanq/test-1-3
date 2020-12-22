5ae91fe005caf23a9b09f04b321e9a0f2256ebfd
mzhaonanq <1002604003@qq.com>
2020-12-22 10:58:20 +0800

    添加backdrop

--- src/index.js
+++ src/index.js
@@ -5,11 +5,26 @@ $("#picker1").picker({
       textAlign: "center",
       values: ["男", "女"]
     }
-  ]
+  ],
+  onClose: function() {
+    $('.backdrop').css('display', 'none');
+  }
 });
 
-$("#input-calendar1").calendar()
-$("#input-calendar2").calendar()
+$("#input-calendar1").calendar(
+  {
+    onClose: function() {
+      $('.backdrop').css('display', 'none');
+    }
+  }
+)
+$("#input-calendar2").calendar(
+  {
+    onClose: function() {
+      $('.backdrop').css('display', 'none');
+    }
+  }
+)
 
 $("#picker2").picker({
   title: "请选择是否已婚",
@@ -18,7 +33,10 @@ $("#picker2").picker({
       textAlign: "center",
       values: ["是", "否"]
     }
-  ]
+  ],
+  onClose: function() {
+    $('.backdrop').css('display', 'none');
+  }
 });
 
 $("#picker3").picker({
@@ -28,9 +46,21 @@ $("#picker3").picker({
       textAlign: "center",
       values: ["随时", "一周内","半个月内","一个月内","待定"]
     }
-  ]
+  ],
+  onClose: function() {
+    $('.backdrop').css('display', 'none');
+  }
 });
 
 $("#my-textarea").on('keyup',function (e) {
   $("#count")[0].innerText=e.target.value.length
 })
+
+$('#input-calendar1,#input-calendar2,#picker1,#picker2,#picker3').click(function() {
+  $('input, textarea').blur();
+  $('.backdrop').css('display', 'block');
+});
+
+$('.backdrop').on('touchmove', function(event) {
+  event.preventDefault();
+});
